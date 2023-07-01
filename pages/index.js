133e2c6509309adb5e2b7b023021566ac638eec2
Ajasra <betinvasilij@gmail.com>
2023-07-01 15:12:57 +0800

    - final

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import { Title } from "@mantine/core";
 import LayoutApp from "components/Layout/layoutApp";
-import { ChatContext, ChatDispatchContext } from "components/Context/context";
+import { ChatContext } from "components/Context/context";
 import { useContext } from "react";
 import ChatPage from "components/Chat/chatPage";
 
-const openai_key = process.env.NEXT_PUBLIC_OPANAI_API;
-
 export default function Home() {
   const chatContext = useContext(ChatContext);
-  const setChatContext = useContext(ChatDispatchContext);
 
   console.log(chatContext);
 
